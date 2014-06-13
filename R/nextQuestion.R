8c748a4426a07553a98622d3b5381933fc73ccf8
Jiun Wei Chia <jiunwei.moe@gmail.com>
2014-06-13 10:35:26 +0800

    Updated 2014-06-13

--- R/nextQuestion.R
+++ R/nextQuestion.R
@@ -1,13 +1,15 @@
 nextQuestion <- function(items, responses, theta) {
+	bank <- cbind(1, questions["difficulty"], 0, 1)
+
 	if (length(items) == 0) {
-	  	return(list(current_item = 50,
-	  		current_item_question = as.character(questions[50, "content"]),
-	  		current_item_answer = as.numeric(questions[50, "correct_answer"]),
+		current_item = sample(startItems(bank, nrItems=5)$items, 1)
+	  	return(list(current_item = current_item,
+	  		current_item_question = as.character(questions[current_item, "content"]),
+	  		current_item_answer = as.numeric(questions[current_item, "correct_answer"]),
 	  		theta = 0.0,
 	  		sem = "N/A"))
 	}
 
-	bank <- cbind(1, questions["difficulty"], 0, 1)
 	theta <- thetaEst(bank[items,], responses)
 	sem <- semTheta(theta, bank[items,])
 	current_item <- nextItem(bank, theta=theta, out=items)$item
